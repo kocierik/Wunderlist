d765947da2fb870716bf6f6b5e0dc6714ab630ee
kocierik <kocierik@gmail.com>
2021-09-16 00:13:43 +0200

    feat: add lorem ipsum

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
 import Stack from "@mui/material/Stack"
 import { Grid } from "@mui/material"
-import Image from "./img/image2.jpg"
-// Import using relative path
+// import Image2 from "./img/image2.jpg"
+
 function Copyright() {
   return (
     <Typography variant="body2" color="white" align="center">
@@ -22,13 +22,6 @@ function Copyright() {
       {new Date().getFullYear()}.
     </Typography>
   )
-}
-
-const styles = {
-  paperContainer: {
-    backgroundImage: `url(${Image})`,
-    height: "400px",
-  },
 }
 
 function App2() {
@@ -51,9 +44,10 @@ function App2() {
         </div> */}
         <Box
           sx={{
-            pt: 0,
-            pb: 0,
+            pt: 8,
+            pb: 50,
           }}
+          p={0}
         >
           <Container maxWidth="25%">
             <Typography
@@ -82,21 +76,52 @@ function App2() {
               spacing={3}
               justifyContent="center"
             >
-              <Button variant="contained">sign up</Button>
+              <Button variant="contained" bgcolor="white">
+                sign up
+              </Button>
               <Button variant="outlined">sign in</Button>
             </Stack>
           </Container>
         </Box>
 
         <div className="content_main">
-          <Grid
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid item xs={12} style={styles.paperContainer} />
-          </Grid>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4} flexDirection="column">
+              <Grid item xs={12} sm={12} md={12}>
+                {/* <img
+                  src={Image2}
+                  alt="ok"
+                  style={{ backgroundSize: "contain" }}
+                /> */}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Grid>
+            </Grid>
+          </Container>
         </div>
       </div>
       {/* Footer */}
